487c230c2ae93162216e91ccc6438c5d58c26a98
unknown <rigan.paul@g.bracu.ac.bd>
2024-10-17 02:14:49 +0600

    updated

--- backend/app.js
+++ backend/app.js
@@ -51,10 +51,7 @@ app.locals.moment = moment;
 
 //db connection
 mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log(
       `DB connected successfully on ${process.env.MONGO_CONNECTION_STRING} `
